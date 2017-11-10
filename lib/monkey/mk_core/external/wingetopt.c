3a44cd0a8c3bf0e04993fc5f2c83816a5e82df24
Ganezu79 <eganea@software.ucv.ro>
2017-11-10 12:09:01 +0200

    Update CMake files

--- lib/monkey/mk_core/external/wingetopt.c
+++ lib/monkey/mk_core/external/wingetopt.c
@@ -19,77 +19,61 @@ Code given out at the 1985 UNIFORUM conference in Dallas.
 	fputs(argv[0], stderr);\
 	fputs(s, stderr);\
 	fputc(c, stderr);}
-	//(void) write(2, argv[0], (unsigned)strlen(argv[0]));\
+//(void) write(2, argv[0], (unsigned)strlen(argv[0]));\
 	//(void) write(2, s, (unsigned)strlen(s));\
 	//(void) write(2, errbuf, 2);}
 
-int     opterr = 1,             /* if error message should be printed */
-optind = 1,             /* index into parent argv vector */
-optopt,                 /* character checked for validity */
-optreset;               /* reset getopt */
-char    *optarg;                /* argument associated with option */
+int	opterr = 1;
+int	optind = 1;
+int	optopt;
+char	*optarg;
 
-#define BADCH   (int)'?'
-#define BADARG  (int)':'
-#define EMSG    ""
-
-/*
-* getopt --
-*      Parse argc/argv argument vector.
-*/
-int getopt(int nargc, char * const nargv[], const char *ostr)
+int
+getopt(argc, argv, opts)
+int	argc;
+char	**argv, *opts;
 {
-    static char *place = EMSG;              /* option letter processing */
-    const char *oli;                        /* option letter list index */
+    static int sp = 1;
+    register int c;
+    register char *cp;
 
-    if (optreset || !*place) {              /* update scanning pointer */
-        optreset = 0;
-        if (optind >= nargc || *(place = nargv[optind]) != '-') {
-            place = EMSG;
-            return (-1);
+    if (sp == 1)
+        if (optind >= argc ||
+            argv[optind][0] != '-' || argv[optind][1] == '\0')
+            return(EOF);
+        else if (strcmp(argv[optind], "--") == NULL) {
+            optind++;
+            return(EOF);
         }
-        if (place[1] && *++place == '-') {      /* found "--" */
-            ++optind;
-            place = EMSG;
-            return (-1);
+        optopt = c = argv[optind][sp];
+        if (c == ':' || (cp = strchr(opts, c)) == NULL) {
+            ERR(": illegal option -- ", c);
+            if (argv[optind][++sp] == '\0') {
+                optind++;
+                sp = 1;
+            }
+            return('?');
         }
-    }                                       /* option letter okay? */
-    if ((optopt = (int)*place++) == (int)':' ||
-        !(oli = strchr(ostr, optopt))) {
-        /*
-        * if the user didn't specify '-' as an option,
-        * assume it means -1.
-        */
-        if (optopt == (int)'-')
-            return (-1);
-        if (!*place)
-            ++optind;
-        if (opterr && *ostr != ':')
-            (void)printf("illegal option -- %c\n", optopt);
-        return (BADCH);
-    }
-    if (*++oli != ':') {                    /* don't need argument */
-        optarg = NULL;
-        if (!*place)
-            ++optind;
-    }
-    else {                                  /* need an argument */
-        if (*place)                     /* no white space */
-            optarg = place;
-        else if (nargc <= ++optind) {   /* no arg */
-            place = EMSG;
-            if (*ostr == ':')
-                return (BADARG);
-            if (opterr)
-                (void)printf("option requires an argument -- %c\n", optopt);
-            return (BADCH);
+        if (*++cp == ':') {
+            if (argv[optind][sp + 1] != '\0')
+                optarg = &argv[optind++][sp + 1];
+            else if (++optind >= argc) {
+                ERR(": option requires an argument -- ", c);
+                sp = 1;
+                return('?');
+            }
+            else
+                optarg = argv[optind++];
+            sp = 1;
         }
-        else                            /* white space */
-            optarg = nargv[optind];
-        place = EMSG;
-        ++optind;
-    }
-    return (optopt);                        /* dump back option letter */
+        else {
+            if (argv[optind][++sp] == '\0') {
+                sp = 1;
+                optind++;
+            }
+            optarg = NULL;
+        }
+        return(c);
 }
 
 #endif  /* __GNUC__ */
